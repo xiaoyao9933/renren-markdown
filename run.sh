b848ffd23f367fdd8956f0734adee2a2d120f18f
xiaoyao9933 <me@chao.lu>
2013-03-15 20:10:22 +0800

    clear

--- run.sh
+++
@@ -1,8 +0,0 @@
-iced -I inline  -c renren-markdown.chrome.iced
-cp -f _include.js release/chrome_plugin/js/renren-markdown.chrome.js
-cat renren-markdown.chrome.js>>release/chrome_plugin/js/renren-markdown.chrome.js
-iced -I inline  -c renren-markdown.user.iced
-cp -f _include.js release/renren-markdown.user.js
-cat renren-markdown.chrome.js>>release/renren-markdown.user.js
-
-
